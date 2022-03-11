23c8a19e3268cb7a8604d9a9955a32d411c961c3
WilterD <wilterd18@gmail.com>
2022-03-11 16:14:00 -0400

    agregar elementos con js

--- index.js
+++ index.js
@@ -16,9 +16,29 @@ document.addEventListener('DOMContentLoaded',function(){
         if(title.value === '' || description.value===''){
         alert.classList.remove('d-none');
         alert.innerText = 'error';
-        }else{
-            console.log('ok');
+        return;
         }
+       const row = table.insertRow();
+       row.innerHTML= `
+
+       <td>${title.value}</td>  
+       <td>${description.value}</td>  
+       <td class="text-center">
+                <input type="checkbox">
+              </td>
+              <td class="text-right">
+                <button class="btn btn-primary mb-1">
+                  <i class="fa fa-pencil"></i>
+                </button>
+                <button class="btn btn-danger mb-1 ml-1">
+                  <i class="fa fa-trash"></i>
+                </button>
+              </td>   
+
+       `;
+        
+        
+        btn.onclick=addTodo;
     }
 
 });
